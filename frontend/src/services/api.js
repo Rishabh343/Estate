75f3900459b54193c527486685f55e42c7e40583
Rishabh343 <sinharishabhkumar8@gmail.com>
2026-07-15 18:42:23 +0530

    Fixed Error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://estate-backend-1xrm.onrender.com/api",
+  baseURL: "http://localhost:8000/api/",
   withCredentials: true,
 });
-// 
+//
 export default api;
+// ("https://estate-backend-1xrm.onrender.com/api");
